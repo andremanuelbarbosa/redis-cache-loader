3a644fdd3774a24af2caf5a260ef89758c4d9c92
Andre Barbosa <andremanuelbarbosa@gmail.com>
2014-01-20 21:09:55 +0000

    Added the first version of the Segment tree implementation.

---
+++ src/main/java/com/andremanuelbarbosa/redis/cacheloader/Segment.java
@@ -0,0 +1,53 @@
+package com.andremanuelbarbosa.redis.cacheloader;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Segment<T> {
+
+    private final Long min;
+    private final Long max;
+    private final T value;
+
+    public Segment(Long min, Long max, T value) {
+
+        this.min = min;
+        this.max = max;
+        this.value = value;
+    }
+
+    public Long getMin() {
+
+        return this.min;
+    }
+
+    public Long getMax() {
+
+        return this.max;
+    }
+
+    public T getValue() {
+
+        return this.value;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
---
+++ src/test/java/com/andremanuelbarbosa/redis/cacheloader/SegmentTreeTest.java
@@ -0,0 +1,65 @@
+package com.andremanuelbarbosa.redis.cacheloader;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SegmentTreeTest {
+
+    @Test
+    public void shouldReturnSizeZeroWhenSegmentsIsNull() {
+
+        SegmentTree<String> segmentTree = new SegmentTree<String>(null);
+
+        Assert.assertEquals(0, segmentTree.getSize());
+    }
+
+    @Test
+    public void shouldReturnSizeOneWhenOneSegmentIsAdded() {
+
+        List<Segment<String>> segments = new LinkedList<Segment<String>>();
+        segments.add(new Segment<String>(1l, 2l, "A"));
+
+        SegmentTree<String> segmentTree = new SegmentTree<String>(segments);
+
+        Assert.assertEquals(1, segmentTree.getSize());
+    }
+
+    @Test
+    public void shouldReturnSizeTwoWhenTwoSpacedSegmentsAreAdded() {
+
+        List<Segment<String>> segments = new LinkedList<Segment<String>>();
+        segments.add(new Segment<String>(1l, 2l, "A"));
+        segments.add(new Segment<String>(4l, 5l, "B"));
+
+        SegmentTree<String> segmentTree = new SegmentTree<String>(segments);
+
+        Assert.assertEquals(2, segmentTree.getSize());
+    }
+
+    @Test
+    public void shouldReturnSizeTwoWhenTwoConsecutiveSegmentsAreAdded() {
+
+        List<Segment<String>> segments = new LinkedList<Segment<String>>();
+        segments.add(new Segment<String>(1l, 2l, "A"));
+        segments.add(new Segment<String>(2l, 3l, "B"));
+
+        SegmentTree<String> segmentTree = new SegmentTree<String>(segments);
+
+        Assert.assertEquals(2, segmentTree.getSize());
+    }
+
+    @Test
+    public void shouldReturnSizeTwoWhenTwoOverlappingSegmentsAreAdded() {
+
+        List<Segment<String>> segments = new LinkedList<Segment<String>>();
+        segments.add(new Segment<String>(1l, 3l, "A"));
+        segments.add(new Segment<String>(2l, 4l, "B"));
+
+        SegmentTree<String> segmentTree = new SegmentTree<String>(segments);
+
+        Assert.assertEquals(2, segmentTree.getSize());
+    }
+}
